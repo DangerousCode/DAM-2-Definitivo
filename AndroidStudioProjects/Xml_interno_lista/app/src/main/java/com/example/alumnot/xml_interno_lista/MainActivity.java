95ddbd01646c40c4679b844e707bbf644ef51333
ax25 <asantosq1@gmail.com>
2015-12-02 20:43:46 +0100

    jolo

--- AndroidStudioProjects/Xml_interno_lista/app/src/main/java/com/example/alumnot/xml_interno_lista/MainActivity.java
+++ AndroidStudioProjects/Xml_interno_lista/app/src/main/java/com/example/alumnot/xml_interno_lista/MainActivity.java
@@ -1,94 +1,52 @@
 package com.example.alumnot.xml_interno_lista;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.content.res.XmlResourceParser;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.view.Menu;
+import android.view.MenuItem;
 
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserException;
+public class MainActivity extends AppCompatActivity {
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-
-/**
- * Created by AlumnoT on 05/11/2015.
- */
-public class MainActivity extends Activity{
-    private ArrayList<Personaje> listaPersonajes = new ArrayList<Personaje>();
-    private ArrayList<String> listaTitulos = new ArrayList<String>();
-    private ArrayList<Personaje> listaPersonajesOrdenados=new ArrayList<Personaje>();
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        try{
-            parsearXML();
-            montarListView();
-        }
-        catch (XmlPullParserException e){
-            e.printStackTrace();
-        }
-        catch (IOException e){
-            e.printStackTrace();
-        }
-    }
-
-    private void parsearXML() throws XmlPullParserException , IOException{
-
-        XmlPullParser parser = getResources().getXml(R.xml.personajes);
-        int eventType = -1;
-
-        while (eventType != XmlResourceParser.END_DOCUMENT){
-            eventType = parser.next();
-            if(eventType == XmlResourceParser.START_TAG){
-                String currentTagName = parser.getName();
-                if(currentTagName.equals("personaje")){
-                    String nombreValue = parser.getAttributeValue(null,"nombre");
-                    String rolValue = parser.getAttributeValue(null,"rol");
-                    String imagen = parser.getAttributeValue(null,"imagen");
-                    String historia=parser.getAttributeValue(null,"historia");
-
-                    Personaje personajeActual = new Personaje(nombreValue.toUpperCase(),rolValue, imagen, historia);
-
-                    listaPersonajes.add(personajeActual);
-                    listaTitulos.add(nombreValue.toUpperCase());
-                }
-            }
-        }
-        //listaTitulos sorted by name
-        Collections.sort(listaTitulos);
-
-        //Algorithm for sort the objects by attribute "nombre"
-        for(int i=0;i<listaTitulos.size();i++){
-            for(int j=0;j<listaPersonajes.size();j++){
-                if(listaTitulos.get(i).equals(listaPersonajes.get(j).getNombre())){
-                    listaPersonajesOrdenados.add(listaPersonajes.get(j));
-                }
-            }
-        }
-    }
-
-    private void montarListView(){
-        ListView listView = (ListView) findViewById(R.id.lista);
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_item,listaTitulos);
-        listView.setAdapter(adapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent i = new Intent(MainActivity.this, DetalleActivity.class);
-
-                i.putExtra("Personaje", listaPersonajesOrdenados.get(position));
-                startActivity(i);
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
